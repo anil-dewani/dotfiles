ec88ccbee916c52a0479503d1a37a0ec776543e5
Anil Dewani <contact@anildewani.com>
2024-10-03 11:19:26 +0530

    adding support for a new monitor to yabai

--- yabai/refresh_displays.sh
+++ yabai/refresh_displays.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Part of the display name to search for
-DISPLAY_NAME_PART="LG ULTRAGEAR"
+DISPLAY_NAME_PART_1="LG ULTRAGEAR"
+DISPLAY_NAME_PART_2="LG HDR 4K"
 
-# Use system_profiler to get display info and check if the display is connected
-if system_profiler SPDisplaysDataType | grep -q "$DISPLAY_NAME_PART"; then
-	yabai -m config left_padding 110
-	yabai -m config right_padding 110
+# Use system_profiler to get display info and check if either display is connected
+if system_profiler SPDisplaysDataType | grep -q "$DISPLAY_NAME_PART_1" || system_profiler SPDisplaysDataType | grep -q "$DISPLAY_NAME_PART_2"; then
+  yabai -m config left_padding 110
+  yabai -m config right_padding 110
 else
-	yabai -m config left_padding 10
-	yabai -m config right_padding 10
+  yabai -m config left_padding 0
+  yabai -m config right_padding 0
 fi
